1809cbe222ef59433b6f84ae8f6e3e15f38b32a9
Enhex <enhex0@gmail.com>
2018-08-18 15:36:45 +0300

    updated network message IDs, since Urho's minimum ID for custom messages was increased.

--- CSP_messages.h
+++ CSP_messages.h
@@ -5,8 +5,8 @@ namespace Urho3D
 	/* Client Side Prediction  Message IDs */
 	/* Client -> server */
 	// Custom input message to add update ID and be in sync with the update rate
-	constexpr int MSG_CSP_INPUT = 32;
+	constexpr int MSG_CSP_INPUT = 153;
 	/* Server -> client */
 	// Sends a complete snapshot of the world
-	constexpr int MSG_CSP_STATE = 33;
+	constexpr int MSG_CSP_STATE = 154;
 }
